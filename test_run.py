d47599d0f1763fe3cee73f557e1a9bc3f0c92776
Chuan-dev086 <choon3406@gmail.com>
2026-02-02 01:42:00 +0800

    feat: 增加访客登记功能，实现数据持久化

--- test_run.py
+++ test_run.py
@@ -1,20 +1,29 @@
-while True:
-    age_input=input ("请输入你的年龄（输入 'q' 退出）:")
+import datetime 
 
-    if age_input.lower()=='q': 
-        print("程序已退出。") 
+print("--- 智能门禁系统 v2.0 ---") 
+
+while True: 
+    name=input("\n请输入访客姓名(输入'q'退出):") 
+    if name.lower()=='q': 
         break 
+
+    age_input=input(f"你好{name},请输入你的年龄：") 
 
     try: 
         age=int(age_input) 
         if age>=18: 
-             print("成年，允许进入。")  
-        else:
-            print("未成年，禁止进入。")  
+            status='已授权'  
+            print(f"✅ {status},欢迎进入。") 
+
+            with open("visitors.txt","a",encoding="utf-8")as file: 
+                now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+                file.write(f"时间：{now}|姓名：{name}|状态：{status}\n") 
+
+        else: 
+            print("❌ 未成年，禁止进入。") 
     except ValueError: 
-        print("⚠️ 输入错误！请输入纯数字。")  
+        print("⚠️ 输入无效，请输入数字。") 
 
-print("--- 感谢使用 ---")
+print("记录已保存至 visitors.txt，再见！") 
 
-   
         
